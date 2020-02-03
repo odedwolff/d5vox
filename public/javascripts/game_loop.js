55a349e72abff1f7888a9014043285690c67b971
unknown <odedwolff@gmail.com>
2020-02-03 10:57:26 +0200

    added computing words' famliarity by user statistics

--- public/javascripts/game_loop.js
+++ public/javascripts/game_loop.js
@@ -1,6 +1,8 @@
 var wordsInfo;
 
 
+
+
 function loadWords(){
 	var xhr = new XMLHttpRequest();
 	//xhr.open('PUT', 'myservice/user/1234');
@@ -12,6 +14,7 @@ function loadWords(){
 			console.log(userInfo);
 			wordsInfo = propsToArray(userInfo);
 			enableGameLoop(true);
+			applyWordsFamiliarity();
 		}else{
 			console.log("reply status:" + xhr.status);
 		}
@@ -38,3 +41,36 @@ function propsToArray(resultsContainer){
 	}
 	return arr; 
 }
+
+//scale: closer to 1 inidicates better familiarity, closer to 0 worse 
+function wordFamiliarity(nmGuesses, nmSuccess){
+	//success factor- rate of good guesses to overall gusses
+	var succFactor;
+	if(nmGuesses==0){
+		succFactor=0;
+	}else{
+		succFactor = nmSuccess / nmGuesses;
+	}
+	//0=>0, then asimpthotically getting at 1 
+	const expFactor =  1-(1/(nmGuesses+1));
+	return expFactor * succFactor;
+}
+
+function applyWordsFamiliarity(){
+	var word; 
+	var familiarity;
+	for(i in wordsInfo){
+		word=wordsInfo[i];
+		
+		if(word.stat){
+			familiarity=wordFamiliarity(word.stat.attemptsCount, word.stat.correctCount);
+			word.familiarity=familiarity;
+		}else{
+			familiarity=wordFamiliarity(0, 0);
+			word.familiarity=familiarity;
+		}
+	}
+}
+
+
+
